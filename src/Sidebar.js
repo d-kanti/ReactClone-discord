5ab63ce8aa62718000cc43041ae3ee2cfcc33be9
d-kanti <dibyakanti22@gmail.com>
2020-10-09 02:39:38 +0530

    Completed the Sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,10 +1,85 @@
 import React from 'react'
 import "./Sidebar.css"
+import AddIcon from '@material-ui/icons/Add';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SidebarChanel from './SidebarChanel';
+import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt';
+import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined';
+import CallIcon from '@material-ui/icons/Call';
+import { Avatar } from '@material-ui/core';
+import MicNoneIcon from '@material-ui/icons/MicNone';
+import HeadsetIcon from '@material-ui/icons/Headset';
+import SettingsIcon from '@material-ui/icons/Settings';
+
+
 
 function Sidebar() {
     return (
         <div className="sidebar">
-            <h1>hello i am a sidebar hihi </h1>
+            
+
+            <div className="sidebar__top">
+                <h3>Cleaver Programmer</h3>
+                <ExpandMoreIcon />
+            </div>
+
+            <div className="sidebar__chanels">
+                <div className="sidebar_chanelsHeader">
+
+                    <div className="sidebar_header">
+                        <ExpandMoreIcon />
+                        <h4>Test Chanels</h4>
+                    </div>
+                    <AddIcon className="sidebar__addChanels"/>
+
+                </div>
+
+                <div className="sidebar__chanelList">
+                    <SidebarChanel />
+                    <SidebarChanel />
+                    <SidebarChanel />
+                    <SidebarChanel />
+
+                </div>
+
+                
+
+
+
+            </div>
+
+            <div className="sidebar__voice">
+                <SignalCellularAltIcon
+                    className="sidebar__voiceIcon"
+                    fontSize="Large"
+                    />
+                
+                <div className="sidebar__voiceInfo">
+                    <h3>Voice Connected</h3>
+                    <p>Stream</p>
+                </div>
+
+                <div className="sidebar__voiceIcons">
+                    <InfoOutlinedIcon />
+                    <CallIcon />
+
+                </div>
+            </div>
+
+            <div className="sidebar__profile">
+                <Avatar src = "https://avatars0.githubusercontent.com/u/64345702?s=460&u=029597a026d7bb18bac354eacb78efa1f1643bf9&v=4"/>
+                <div className="sidebar__profileInfo">
+                    <h3>My Name This</h3>
+                    <p>ID:4568321</p>
+                </div>
+                <div className="sidebar__profileIcons">
+                    <MicNoneIcon />
+                    <HeadsetIcon />
+                    <SettingsIcon />
+                    
+                </div>
+            </div>
+
         </div>
     )
 }
---
+++ src/SidebarChanel.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./SidebarChanel.css"
+
+
+function SidebarChanel({ id, channel}) {
+    return <div className="sidebarChanel">
+            <h4 >
+                <span className="sidebarChanel__hash">#</span>    YouTube
+            </h4>
+        </div>
+    
+}
+
+export default SidebarChanel
