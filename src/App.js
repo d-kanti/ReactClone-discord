76795f4918cebc71b22d69a9e730feb95ba5aaa7
d-kanti <dibyakanti22@gmail.com>
2020-10-09 01:03:30 +0530

    created sidebar.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import { Counter } from './features/counter/Counter';
+
 import './App.css';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     //BEM naming convention
     <div className="app">
-        <h1> lets build</h1>    
+         
+
+        {/*Sidebar*/}
+        <Sidebar />
+
+        {/*Chat*/}
+
+
      </div>
   );
 }
---
+++ src/Sidebar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Sidebar.css"
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <h1>hello i am a sidebar hihi </h1>
+        </div>
+    )
+}
+
+export default Sidebar
